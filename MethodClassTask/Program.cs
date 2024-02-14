fb7ed1900cb3872cd3fc72a1fccda38b590334a2
Fatima E. Gaykhanova <fatimaeg@code.local>
2024-02-14 12:39:38 +0400

    Added Some Method Tasks at Class

---
+++ MethodClassTask/Program.cs
@@ -0,0 +1,93 @@
+﻿//string GetTemprature(int temp)
+//{
+
+
+//    if (temp > 20)
+//    {
+//        return "Isti";
+//    }
+//    else if (temp < 20)
+//    {
+//        return "Soyuq";
+//    }
+//    else
+//    {
+//        return "Mulayim";
+//    }
+
+
+//}
+//int temp = int.Parse(Console.ReadLine());
+
+//string result = GetTemprature(temp);
+//Console.WriteLine(result);
+
+//int GetNumbersCount(int a,int b)
+//{
+//    int count = 0;
+
+//    for (int i = a; i <b; i++)
+//    {
+//        count++;
+//    }
+
+//    return count;
+//}
+
+//int result = GetNumbersCount(5,10);
+//Console.WriteLine(result);
+
+//int SumOfArray( int[] arr1, int[] arr2)
+//{
+//    int sum = 0;
+    
+
+//    for (int i = 0; i < arr1.Length; i++)
+//    {
+//        sum+= arr1[i];
+//    }
+    
+
+//    for (int i = 0; i < arr2.Length; i++)
+//    {
+//        sum += arr2[i];
+//    }
+
+   
+//    return sum;
+
+//}
+
+//int[] arr1 = { 1, 2, 3, 4, 5 };
+//int[] arr2 = { 2, 3, 3, 7, 8 };
+
+//int finalResult= SumOfArray(arr1 , arr2);
+//Console.WriteLine(finalResult);
+
+//int PowerOfNUmber(int n)
+//{
+//    int power = n * n;
+//    return power;
+//}
+
+//int result3= PowerOfNUmber(10);
+//Console.WriteLine(result3);
+
+//int SumOfNumbers(int n)
+//{ 
+//    int sum = 0;
+//    for (int i = 1;i <=n;i++)
+//    {
+//        if (i%2==0)
+//        {
+//            sum += i;
+//        }
+
+       
+//    }
+//    return sum;
+
+//}
+//int result4 = SumOfNumbers(10);
+//Console.WriteLine(result4);
+
